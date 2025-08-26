a50d1190c2ef8d9f40abad45c527042bb30ee94d
Nicholas Hancock <nick.hancock777@gmail.com>
2025-08-25 22:21:41 -0500

    Added user class & testing

--- src/user.ts
+++ src/user.ts
@@ -1,22 +1,39 @@
+import { Chapter } from "~/chapter";
 export class User {
-  storage: Record<string, string>;
+  owned_stories: Set<string>;
   credits: number;
 
-  constructor(storage: Record<string, string>) {
-    this.storage = storage;
+  constructor(owned_stories: Set<string>) {
+    this.owned_stories = owned_stories;
     this.credits = 0;
   }
-
   has_credits(cost: number): boolean {
     return this.credits >= cost;
   }
-
   add_credits(credits: number) {
     this.credits += credits;
   }
-
+  owns(now: number, chapter: Chapter): boolean {
+    if (chapter.is_free(now)) return true;
+    if (this.owned_stories.has(chapter.name)) return true;
+    return false;
+  }
+  own(now: number, chapter: Chapter): boolean {
+    if (this.owns(now, chapter)) return true;
+    if (this.credits >= chapter.cost) {
+      this.owned_stories.add(chapter.name);
+      this.credits -= chapter.cost;
+      return true;
+    }
+    if (this.payment_flow()) {
+      this.owned_stories.add(chapter.name);
+      this.credits -= chapter.cost;
+    }
+    return false;
+  }
   // Model paying out to stripe and returning new credits
-  payment_flow(): number {
-    return 500;
+  payment_flow(): boolean {
+    this.credits += 500;
+    return true;
   }
 }
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,15 +1,48 @@
 import { describe, expect, test } from "vitest";
+import { Chapter } from "~/chapter";
 import { User } from "~/user";
 
-describe("User math", () => {
-  test("User defaults to zero credits", () => {
-    let user = new User({});
+describe("User", () => {
+  test("defaults to zero credits", () => {
+    let user = new User(new Set());
     expect(user.has_credits(1)).toBe(false);
   });
-  test("User credit math", () => {
-    let user = new User({});
+  test("credit math works", () => {
+    let user = new User(new Set());
     expect(user.has_credits(1)).toBe(false);
     user.add_credits(1);
     expect(user.has_credits(1)).toBe(true);
+  });
+});
+describe("User ownership", () => {
+  let r2 = {};
+  let expired_chapter = new Chapter(r2, "expired", 0, 3);
+  let active_chapter = new Chapter(r2, "active", 9999999999, 1);
+  let expensive_chapter = new Chapter(r2, "active", 9999999999, 50);
+
+  test("owns expired chapters for free", () => {
+    let user = new User(new Set());
+    expect(user.owns(0, expired_chapter)).toBe(true);
+  });
+  test("doesn't own active chapters for free", () => {
+    let user = new User(new Set());
+    expect(user.owns(0, active_chapter)).toBe(false);
+  });
+  test("can buy chapters it can afford", () => {
+    let user = new User(new Set());
+    user.add_credits(1);
+    expect(user.own(0, active_chapter)).toBe(true);
+  });
+  test("can't buy chapters it can't afford", () => {
+    let user = new User(new Set());
+    user.add_credits(1);
+    expect(user.own(0, expensive_chapter)).toBe(false);
+  });
+  test("correctly verifies bought chapters", () => {
+    let user = new User(new Set());
+    user.add_credits(1);
+    user.own(0, active_chapter);
+    expect(user.owns(0, active_chapter)).toBe(true);
+    expect(user.own(0, active_chapter)).toBe(true);
   });
 });
